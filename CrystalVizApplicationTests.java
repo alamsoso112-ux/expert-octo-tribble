d7dd17d4d009c57c0b17aa00de7579c2fcad3796
alamsoso112-ux <alamsoso112@gmail.com>
2026-02-07 22:06:56 +0800

    Add files via upload

---
+++ CrystalVizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brother.crystalviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalVizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
